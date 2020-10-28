4dc14837fea86dae254ef5796a7f3c245926bb3b
sampanchan <spankratz@my.creativecircus.edu>
2020-10-28 12:32:27 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log('hello')
+
+
+// have only one question on screen
+// have radio-buttons select and deselect answer
+// have question answered before going onto the next question
+// have next button go to next question
+// have value of question calculated
+// last question/section be what you are (wizard)
+
+
+
+var wizardQuestions = document.querySelectorAll('section')
+
+let nextButton = document.querySelectorAll('button')
+    
+
+nextButton.forEach(function(button){
+button.addEventListener= ('click', function({
+    
+}))
+}
+    {
+    
+});
+
+
+
